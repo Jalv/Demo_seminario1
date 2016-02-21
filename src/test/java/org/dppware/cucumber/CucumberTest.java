7e900257c87a38033a65511a48f6f321c98cd461
juan <juanevz@gmail.com>
2016-02-22 00:26:35 +0100

    cambio a json2

--- src/test/java/org/dppware/cucumber/CucumberTest.java
+++ src/test/java/org/dppware/cucumber/CucumberTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"json:target/cucumber"})
+@Cucumber.Options(format = {"json:target/cucumber.json"})
 public class CucumberTest {
 
 }
